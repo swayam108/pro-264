0a8cd20382fe5b4f1b5552108e980a5b7d500d84
swayam108 <79143263+swayam108@users.noreply.github.com>
2024-10-12 16:07:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+#project!!
+# Program to Upload Color Image and convert into Black & White image
+import os
+from flask import  Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+# Write load_form function below to Open and redirect to default upload webpage
+@app.route('/')
+def load_form():
+    return render_template('upload.html')
+
+
+# Write upload_image Function to upload image and redirect to new webpage
+@app.route('/gray',methods=['POST'])
+def upload_image():
+    file=request.files['file']
+    filename=secure_filename(file.filename)
+    file.save(os.path.join('static/',filename))
+
+    display_message="image sucessfully uploaded and displayed below"
+    return render_template("upload.html",filename=filename,message=display_message)
+# Write display_image Function to display the uploaded image
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static',filename=filename))
+
+if __name__ == "__main__":
+    app.run()
